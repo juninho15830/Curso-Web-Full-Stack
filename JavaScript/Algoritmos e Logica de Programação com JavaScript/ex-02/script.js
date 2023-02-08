e07d537be418c3c33961f918758d52fce3f8f983
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-08 20:08:17 -0300

    ex-02 js

---
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/ex-02/script.js
@@ -0,0 +1,7 @@
+alert("Vamos somar 2 números!")
+
+let numberOne = prompt("Primeiro número:")
+let numberTwo = prompt("Segundo número:")
+let result = Number(numberOne) + Number(numberTwo)
+
+alert(`O resultado final é ${result}.`)
