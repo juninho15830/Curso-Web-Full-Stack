251fe63ddb16e601d238cb37656ba2e76c02712f
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-10 21:36:45 -0300

    update jogo adivinhação

--- JavaScript/Algoritmos e Logica de Programação com JavaScript/06-jogo da adivinhação/script.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/06-jogo da adivinhação/script.js
@@ -4,6 +4,18 @@ const randomNumber = Math.round(Math.random() * 10)
 //Math é um pacote com funcionalidades matemáticas, .random gera um número real aleatório entre 0 e 1 não incluindo ambos.
 //.round arredonda um número real tanto para cima quanto para baixo o tranformando em um número inteiro. Exemplo 1.5 se torna 2, e 1.4 se torna 1.
 
-while(Number(result) != randomNumber) {
+let attempts = 1
+let attempt = ""
 
+while(Number(result) != randomNumber) {
+    result = prompt("Erro, tente novamente:")
+    attempts++
 }
+
+if (attempts == 1) {
+    attempt = "tentativa"
+} else {
+    attempt = "tentativas"
+}
+
+alert(`Parabéms! Você adivinhou o número em ${attempts} ${attempt}!`)
