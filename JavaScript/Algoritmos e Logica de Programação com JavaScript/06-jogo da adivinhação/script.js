d3b0e0309cdcde3bb60c6f5e6953036069c156fd
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-12 09:59:48 -0300

    update jogo adivinhação

--- JavaScript/Algoritmos e Logica de Programação com JavaScript/06-jogo da adivinhação/script.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/06-jogo da adivinhação/script.js
@@ -1,21 +1,41 @@
-let result = prompt("Adivinhe o número que estou pensando? Está entre 0 e 10")
+/* 
+    ** Jogo da adivinhação
+    
+    Apresente um mensagem ao usuário:
+    "Adivinhe o número que estou pensando? Está entre 0 e 10"
+    
+    Crie uma lógica para gerar um número aleatório e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+    
+    Enquanto o usuário não adivinhar o número, mostrar a mensagem:
+    "Erro, tente novamente:"
+    
+    Caso o usuário acerte o número, apresentar a mensagem:
+    "Parabéns! Você adivinhou o número em x tentativas"
+    
+    Substitua o "x" da mensagem, pelo número de tentativas
 
-const randomNumber = Math.round(Math.random() * 10) 
+    Caso o usuário acerte o número na primeira tentativa substitua a palavra "tentativas" por "tentativa"
+*/
+
 //Math é um pacote com funcionalidades matemáticas, .random gera um número real aleatório entre 0 e 1 não incluindo ambos.
 //.round arredonda um número real tanto para cima quanto para baixo o tranformando em um número inteiro. Exemplo 1.5 se torna 2, e 1.4 se torna 1.
+
+let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
+
+const randonNumber = Math.round(Math.random() * 10)
 
 let attempts = 1
 let attempt = ""
 
-while(Number(result) != randomNumber) {
+while (Number(result) != randonNumber){
     result = prompt("Erro, tente novamente:")
     attempts++
 }
 
-if (attempts == 1) {
+if (attempts == 1){
     attempt = "tentativa"
 } else {
     attempt = "tentativas"
 }
 
-alert(`Parabéms! Você adivinhou o número em ${attempts} ${attempt}!`)
+alert(`Parabéns! Você adivinhou o número em ${attempts} ${attempt}`)
