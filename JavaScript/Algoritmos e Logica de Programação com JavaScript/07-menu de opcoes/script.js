f349e46658f66a708bcf4a60721d497ced79009e
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-14 07:21:26 -0300

    create ex 07

---
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/07-menu de opcoes/script.js
@@ -0,0 +1,20 @@
+/*
+
+    Faça um programa que tenha um menu e apresente a seguinte mensagem:
+
+    Olá usuário! Digite a opção desejada
+
+        1. Cadastrar um item na lista
+        2. Mostrar itens cadastrados
+        3. Sair do programa
+
+    ---
+    O programa deverá capturar o número digitado pelo usuário e mostrar os seguintes cenários:
+
+    Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
+    Caso o usuário digite 2, ele poderá ver os itens cadastrados
+        Se não houver itens cadastrados, mostrar a mensagem:
+            "Não existem itens cadastrados"
+    Caso o usuário digite 3, a aplicação deverá ser encerrada.
+    
+*/
