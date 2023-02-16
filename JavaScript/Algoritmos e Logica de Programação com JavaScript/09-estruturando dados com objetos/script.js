e10a592a3c7ae2bd6a1df1fff7b090666f5567f8
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-16 07:09:10 -0300

    create ex 09 js

---
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/09-estruturando dados com objetos/script.js
@@ -0,0 +1,41 @@
+let patients = [
+    {
+        name:"Juninho",
+        age: 37,
+        weight: 70,
+        height: 165,
+    },
+    {
+        name:"Juliana",
+        age: 34,
+        weight: 60,
+        height: 162,
+    },
+    {
+        name:"Miguel",
+        age: 11,
+        weight: 42,
+        height: 150,
+    },
+    {
+        name:"Arthur",
+        age: 4,
+        weight: 17,
+        height: 80,
+    }
+]
+
+let patientsNames = []
+let patientsAge = []
+let patientsWeight = []
+let patientsHeight = []
+let patient
+
+for(patient of patients) {
+    patientsNames.push(patient.name)
+    patientsAge.push(patient.age)
+    patientsWeight.push(patient.weight)
+    patientsHeight.push(patientsHeight)
+}
+
+alert(patientsNames)
