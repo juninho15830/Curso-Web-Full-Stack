e0279af7a97c3b7ae73ab625dd475a37dac8c666
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-02-23 20:01:47 -0300

    update

--- JavaScript/Algoritmos e Logica de Programação com JavaScript/09-estruturando dados com objetos/script.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/09-estruturando dados com objetos/script.js
@@ -4,38 +4,51 @@ let patients = [
         age: 37,
         weight: 70,
         height: 165,
+        sexo: "M",
     },
     {
         name:"Juliana",
         age: 34,
         weight: 60,
         height: 162,
+        sexo: "F",
     },
     {
         name:"Miguel",
         age: 11,
         weight: 42,
         height: 150,
+        sexo: "M"
     },
     {
         name:"Arthur",
         age: 4,
-        weight: 17,
-        height: 80,
+        weight: 18,
+        height: 102,
+        sexo: "M"
     }
 ]
 
+let sexo
+
 let patientsNames = []
-let patientsAge = []
-let patientsWeight = []
-let patientsHeight = []
-let patient
 
 for(patient of patients) {
     patientsNames.push(patient.name)
-    patientsAge.push(patient.age)
-    patientsWeight.push(patient.weight)
-    patientsHeight.push(patientsHeight)
+    alert(patientsNames)
 }
 
-alert(patientsNames)
+function printPatient(patient) {
+    if (patient.sexo == "M") {
+        sexo = "O"
+    } else {
+        sexo = "A"
+    }
+    return`
+    ${sexo} paciente ${patient.name} tem ${patient.age} anos, pesa ${patient.weight} quilos e tem ${patient.height} centímetros de altura.`
+}
+
+for (let patient of patients) {
+    let dataPatient = printPatient(patient)
+    alert(dataPatient)
+}
