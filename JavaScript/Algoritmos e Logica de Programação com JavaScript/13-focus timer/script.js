9818e0099787f9cbcff9ec008c7095083f0a3839
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-08 19:47:28 -0300

    create script and button events

---
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/13-focus timer/script.js
@@ -0,0 +1,35 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff= document.querySelector('.sound-off')
+
+buttonPlay.addEventListener('click', function() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+})
+
+buttonPause.addEventListener('click', function() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+    buttonSoundOff.classList.add('hide')
+    buttonSoundOn.classList.remove('hide')
+})
