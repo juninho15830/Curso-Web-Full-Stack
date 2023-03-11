c0da26b43f3696ba89aa63ee4f0850aca84eb2a8
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-10 21:48:41 -0300

    refactoring js

---
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/13-focus timer/modules/timer.js
@@ -0,0 +1,59 @@
+export default function Timer({ //dependencias
+    minutesDisplay,
+    secondsDisplay,
+    resetControls,
+}) {
+
+    let timerTimeOut
+    let minutes = Number(minutesDisplay.textContent)
+    
+    function updateDisplay(minutes, seconds) {
+        minutesDisplay.textContent = String(minutes).padStart(2, "0")
+        secondsDisplay.textContent = String(seconds).padStart(2, "0")//convertendo para string eu consigo com padStart preencher uma casa antes dos numeros 9 ao 1. (2, é o tanto de casas e "0" é a string que quero usar)
+    }
+    
+    function reset() {
+        updateDisplay(minutes, 0)
+        clearTimeout(timerTimeOut)
+    }
+    
+    function countdown() {
+        timerTimeOut = setTimeout(function() {
+            let seconds = Number(secondsDisplay.textContent)
+            let minutes = Number(minutesDisplay.textContent)
+    
+            updateDisplay(minutes, 0)
+    
+            if (minutes <= 0) {
+                resetControls()    
+                return
+            }
+    
+            if (seconds <= 0) {
+                seconds = 60
+                --minutes
+            }
+        
+            updateDisplay(minutes, String(seconds - 1))
+    
+            countdown()
+        }, 1000)
+    }
+
+
+    function updateMinutes(newMinutes) {
+        minutes = newMinutes
+    }
+    
+    function hold() {
+        clearTimeout(timerTimeOut)
+    }
+
+    return {
+        countdown,
+        reset,
+        updateDisplay,
+        updateMinutes,
+        hold
+    }
+}//Este é um padrão de projeto chamado FACTORY, aqui foram colocadas todas as funções de timer, dentro de uma unica função chamda Timer, retornamos um objeto com as funções internas e injetamos as dependencias no começo. 
--- JavaScript/Algoritmos e Logica de Programação com JavaScript/13-focus timer/script.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/13-focus timer/script.js
@@ -1,73 +1,42 @@
+import Controls from "./modules/controls.js"
+import Timer from "./modules/timer.js" //importamos a função factory
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff= document.querySelector('.sound-off')
-let timerTimeOut
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
-let minutes = Number(minutesDisplay.textContent)
 
-function resetControls() {
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-}
+const controls = Controls({
+    buttonPause,
+    buttonPlay,
+    buttonSet,
+    buttonStop
+})
 
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")//convertendo para string eu consigo com padStart preencher uma casa antes dos numeros 9 ao 1. (2, é o tanto de casas e "0" é a string que quero usar)
-}
-
-function resetTimer() {
-    updateTimerDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
-}
-
-function countdown() {
-    timerTimeOut = setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        updateTimerDisplay(minutes, 0)
-
-        if (minutes <= 0) {
-            resetControls()    
-            return
-        }
-
-        if (seconds <= 0) {
-            seconds = 3
-            --minutes
-        }
-    
-        updateTimerDisplay(minutes, String(seconds - 1))
-
-        countdown()
-    }, 1000)
-}
+const timer = Timer({ //aqui criamos uma variavel que recebe a função importada e injetamos as dependencias.
+    minutesDisplay, 
+    secondsDisplay,
+    resetControls: controls.reset
+})
 
 buttonPlay.addEventListener('click', function() {
-    buttonPlay.classList.add('hide')
-    buttonPause.classList.remove('hide')
-    buttonSet.classList.add('hide')
-    buttonStop.classList.remove('hide')
-
-    countdown()
+    controls.play()
+    timer.countdown()
 
 })
 
 buttonPause.addEventListener('click', function() {
-    buttonPause.classList.add('hide')
-    buttonPlay.classList.remove('hide')
-    clearTimeout(timerTimeOut)
+    controls.pause()
+    timer.hold()
 })
 
 buttonStop.addEventListener('click', function() {
-    resetControls()
-    resetTimer()
+    controls.reset()
+    timer.reset()
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -81,12 +50,13 @@ buttonSoundOn.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-    let newMinutes = prompt('Quantos minutos?')
+    let newMinutes = controls.getMinutes()
+
     if (!newMinutes) {
-        resetTimer()
+        timer.reset()
         return
     }
 
-    minutes = newMinutes
-    updateTimerDisplay(minutes, 0)
+    timer.updateDisplay(newMinutes, 0)
+    timer.updateMinutes(newMinutes)
 })
