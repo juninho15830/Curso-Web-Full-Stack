629a8dd05f93e598b8852d35b728b52e591857b8
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-19 10:03:11 -0300

    update js

--- JavaScript/Algoritmos e Logica de Programação com JavaScript/14-github favorites/js/favorites.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/14-github favorites/js/favorites.js
@@ -2,6 +2,26 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+
+    load() {
+        this.entries = [
+            {
+            login: 'juninho15830',
+            name: "Juninho",
+            public_repos: '15',
+            followers: '5432'
+            },
+            {
+                login: 'maykbrito',
+                name: "Mayk Brito",
+                public_repos: '76',
+                followers: '120000'
+            }
+        ]
+
+
     }
 }
 
@@ -10,17 +30,57 @@ export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
 
+        this.tbody = this.root.querySelector('table tbody')
+
         this.update()
     }
 
     update() {
         this.removeAllTr()
+
+        this.entries.forEach( user => {
+            const row = this.creatRow()
+    
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            this.tbody.append(row)
+        })
     }
     
-    removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
+    creatRow() {
+        const tr = document.createElement('tr')
 
-        tbody.querySelectorAll('tr')
+        tr.innerHTML = `
+        <td class="user">
+            <img src="https://github.com/juninho15830.png" alt="Imagem de Juninho">
+            <a href="https://github.com/juninho15830" target="_blank">
+                <p>Juninho</p>
+                <span>juninho15830</span>
+            </a>
+        </td>
+        <td class="repositories">
+            15
+        </td>
+        <td class="followers">
+            5432
+        </td>
+        <td>
+            <button class="remove">&times;</button>
+        </td>
+    </tr>
+        `
+
+        return tr
+    }
+
+    removeAllTr() {
+
+        this.tbody.querySelectorAll('tr')
         .forEach((tr) => {
             tr.remove()
         })
