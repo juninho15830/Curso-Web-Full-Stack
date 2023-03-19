2c33ac3f7835ce731bd81e1a351e42746283dd19
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-19 10:33:01 -0300

    update js

--- JavaScript/Algoritmos e Logica de Programação com JavaScript/14-github favorites/js/favorites.js
+++ JavaScript/Algoritmos e Logica de Programação com JavaScript/14-github favorites/js/favorites.js
@@ -23,6 +23,13 @@ export class Favorites {
 
 
     }
+
+    delete(user) { // filter é uma Higher-order function que serve para filtrar um elemento dentro do array.
+        const filteredEntries = this.entries
+        .filter(entry => entry.login !== user.login)
+
+        console.log(filteredEntries)
+    }
 }
 
 //classe que cria a visualização e os eventos do html.
@@ -47,6 +54,14 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja retornar essa linha?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
 
             this.tbody.append(row)
         })
