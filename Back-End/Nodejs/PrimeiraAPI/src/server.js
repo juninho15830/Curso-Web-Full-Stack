3389fefc5852dd6b3ce84d7407c9cd6919e25ded
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-26 09:10:33 -0300

    update server.js

--- Back-End/Nodejs/PrimeiraAPI/src/server.js
+++ Back-End/Nodejs/PrimeiraAPI/src/server.js
@@ -2,5 +2,19 @@ const express = require("express");//Importa o express
 
 const app = express();//Inicializa o Express
 
+app.get("/mensage/:id/:user", (request, response) => {
+    response.send(`
+        Mensagem ID: ${request.params.id}.
+        Para o usuário: ${request.params.user}.
+    `);
+});
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`);
+
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
