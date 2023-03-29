994f4d84c90420cee0d31d3d61d244349173395e
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-29 20:54:18 -0300

    update routes

--- Back-End/Nodejs/PrimeiraAPI/src/server.js
+++ Back-End/Nodejs/PrimeiraAPI/src/server.js
@@ -1,13 +1,11 @@
 const express = require("express");//Importa o express
 
+const routes = require("./routes")
+
 const app = express();//Inicializa o Express
 app.use(express.json());
 
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
-
-    response.json({ name, email, password });
-});
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
