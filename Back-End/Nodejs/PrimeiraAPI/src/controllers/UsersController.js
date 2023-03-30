dd760d601e41567204560d40cc1c493acbfec8f6
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-03-29 21:04:32 -0300

    Create UsersController.js

---
+++ Back-End/Nodejs/PrimeiraAPI/src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+    /*
+    * index - GET para listar vários registros.
+    * show - GET para exibir um registro específico.
+    * create - POST para criar um registro.
+    * update - PUT para atualizar o registro.
+    * delete - DELETE para remover um registro.
+    */
+}
+
+module.exports = UsersController;
