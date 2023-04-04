7533c6e76f89aac8434378db0c1215e2994b413a
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-04 15:12:56 -0300

    create encrypted password

--- Back-End/Nodejs/PrimeiraAPI/src/controllers/UsersController.js
+++ Back-End/Nodejs/PrimeiraAPI/src/controllers/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require("../database/sqlite") 
 
 class UsersController {
@@ -13,7 +13,9 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [ name, email, password])
+        const hashedPassword = await hash(password, 8); //8 é o fator de complexidade da criptografia
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [ name, email, hashedPassword])
 
         return response.status(201).json();
     }
