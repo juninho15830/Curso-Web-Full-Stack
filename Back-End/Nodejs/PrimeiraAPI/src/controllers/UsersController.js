46b4b6349dafab95b10eb618ec0923626f366319
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-04 14:46:08 -0300

    update usersController

--- Back-End/Nodejs/PrimeiraAPI/src/controllers/UsersController.js
+++ Back-End/Nodejs/PrimeiraAPI/src/controllers/UsersController.js
@@ -13,6 +13,8 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [ name, email, password])
+
         return response.status(201).json();
     }
 
