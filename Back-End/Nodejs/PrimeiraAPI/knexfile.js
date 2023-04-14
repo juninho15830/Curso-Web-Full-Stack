a6213aeafbd3dc15e534c7eaeb21ac5729d42166
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-13 21:18:25 -0300

    create tables

--- Back-End/Nodejs/PrimeiraAPI/knexfile.js
+++ Back-End/Nodejs/PrimeiraAPI/knexfile.js
@@ -6,6 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")//Aqui colocamos o caminho do nosso db.
     },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")//Aqui colocamos o caminho da nossa migration, que é responsavel pelo versionamento do nosso banco.
     },
