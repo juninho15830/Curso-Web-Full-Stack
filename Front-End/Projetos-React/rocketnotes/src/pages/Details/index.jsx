697fcc0301ee31421cd011c89d433873bee8ef32
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-23 09:46:31 -0300

    css in js

--- Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import "./styles.css"
+import { Container } from "./styles";
 
 export function Details(){
 
   return( //Conteúdo
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <p>Meu nome é Junior</p>
-    </>
+    </Container>
   )
 }
---
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: rgb(79, 79, 209);
+`;
