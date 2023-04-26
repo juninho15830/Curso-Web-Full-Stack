ca7aefd808b1f228d7bb5d82493dde9d8f0c6113
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-26 07:18:31 -0300

    update styles

--- Front-End/Projetos-React/rocketnotes/src/main.jsx
+++ Front-End/Projetos-React/rocketnotes/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
 
-import { Details } from './pages/Details'
+import theme from './styles/theme';
+
+import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>,
