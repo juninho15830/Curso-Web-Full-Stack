98585fbb9a2b6183ba00a2d4c12dd0df2efe1530
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-28 20:14:04 -0300

    create children

--- Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,21 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details(){
 
   return( //Conteúdo
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+        </Links>
+      </Section>
 
       <Button title="Voltar"/>
     </Container>
--- Front-End/Projetos-React/rocketnotes/src/pages/Details/styles.js
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/styles.js
@@ -9,5 +9,16 @@ export const Container = styled.div`
     grid-template-areas: 
     "header"
     "content"
-    ;
+`;
+
+export const Links = styled.ul`
+    list-style: none;
+
+    > li {
+        margin-top: 12px;
+
+        a {
+            color: ${({ theme }) => theme.COLORS.WHITE};
+        }
+    }
 `;
