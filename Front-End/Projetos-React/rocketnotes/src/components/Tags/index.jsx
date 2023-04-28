b640e5dad7b5491039185fe7beed5032c5beb7b3
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-28 20:32:42 -0300

    create tags

---
+++ Front-End/Projetos-React/rocketnotes/src/components/Tags/index.jsx
@@ -0,0 +1,8 @@
+import { Container } from "./styles";
+
+export function Tag({ title }) {
+    return (
+        <Container>
+            {title}
+        </Container>    )
+}
--- Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tags";
 
 export function Details(){
 
@@ -15,6 +16,11 @@ export function Details(){
           <li><a href="#">https://www.rocketseat.com.br</a></li>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodejs"/>
       </Section>
 
       <Button title="Voltar"/>
