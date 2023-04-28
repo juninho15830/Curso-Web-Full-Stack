5d7f641f59685969df3153e21a3edf524c2a5dca
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-27 21:20:17 -0300

    update header

--- Front-End/Projetos-React/rocketnotes/src/components/Header/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles";
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
     return (
         <Container>
+            <Profile>
+                <img src="https://github.com/juninho15830.png" alt="Foto do usuário"/>
 
+                <div>
+                    <span>Bem vindo</span>
+                    <strong>Juninho</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
         </Container>
     )
 };
+
