686b52fa4c78a468ae258538a193d60bc8143e5b
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-04-28 21:15:18 -0300

    create scrollbar

--- Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
@@ -1,29 +1,44 @@
-import { Container, Links } from "./styles";
 
-import { Header } from "../../components/Header";
-import { Button } from "../../components/Button";
-import { Section } from "../../components/Section";
+import { Container, Links, Content } from "./styles";
+
 import { Tag } from "../../components/Tags";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details(){
 
   return( //Conteúdo
     <Container>
-      <Header />
+      <Header /> 
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+      <main> 
+        <Content> {/*estas tags main e content server para fazermos uma barra de rolagem no conteúdo sem rolar o header, fizemos isso atravez do arquivo styles.jsx */}
+          <ButtonText title="Excluir nota"/>
 
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/>
-      </Section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Voltar"/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure praesentium aperiam corporis iusto voluptatum, autem alias quibusdam vel tempora ab repellat excepturi laborum quos unde voluptatem voluptatibus eveniet officia?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat sit molestias eaque fuga quasi praesentium, reiciendis nemo unde eveniet quae maiores voluptatibus distinctio, ipsam magnam iste culpa deserunt nisi aut.</p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+
+          <Button title="Voltar"/>
+        </Content>
+      </main>
+
     </Container>
   )
 }
