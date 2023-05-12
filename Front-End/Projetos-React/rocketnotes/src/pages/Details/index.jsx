3774bd180bff51a5cd6a88beb074e0c00f92518f
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-05-11 22:03:49 -0300

    Create Routes

--- Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Details/index.jsx
@@ -1,7 +1,7 @@
 
 import { Container, Links, Content } from "./styles";
 
-import { Tag } from "../../components/Tags";
+import { Tag } from "../../components/Tag";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
--- Front-End/Projetos-React/rocketnotes/src/pages/Home/index.jsx
+++ Front-End/Projetos-React/rocketnotes/src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Note } from '../../components/Note'
-import { Input } from '../../components/Imput';
+import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText';
