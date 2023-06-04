b2d8f15bd9d2ad3c43e8ab2031981ddbee9fd2c3
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-06-04 10:17:08 -0300

    create API

--- Back-End/API/src/server.js
+++ Back-End/API/src/server.js
@@ -1,33 +1,6 @@
-require("express-async-errors");
+const express = require("express");
 
-const migrationsRun = require("./database/sqlite/migrations")
-const AppError = require("./utils/AppError");
-
-const express = require("express");//Importa o express
-const routes = require("./routes");//está carregando o arquivo index.js por padrão
-
-migrationsRun();
-
-const app = express();//Inicializa o Express
-app.use(express.json());
-
-app.use(routes);
-
-app.use(( error, request, response, next ) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        });
-    }
-
-    console.error(error);
-
-    return response.status(500).json({
-        status: "error",
-        message: "Internal server error",
-    });
-});
+const app = express();
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
