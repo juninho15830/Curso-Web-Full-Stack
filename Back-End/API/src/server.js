66e1f90c23ba8735ca314034ea66b6f40e462ee1
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-07-13 20:23:21 -0300

    Create The Witcher Site

--- Back-End/API/src/server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
--- Front-End/Projetos-React/x-men/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function Header() {
-    return(
-        <Container>
-            <img src="../src/assets/images/logo.svg" alt="Logo" />
-        </Container>
-    )
-}
