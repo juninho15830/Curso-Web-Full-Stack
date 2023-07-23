524c17c216939129aeb4b61fc6ff7d8a78b3fab1
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-07-23 08:14:02 -0300

    create db

--- Back-End/API-rocketnotes/src/server.js
+++ Back-End/API-rocketnotes/src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
-
+const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
+
 const express = require("express");
 
 const routes = require("./routes");
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use(( error, request, response, next ) => {
     if(error instanceof AppError) {
