4f25836c8cb8916310787b867710832e9a880eb1
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-07-27 20:01:39 -0300

    create tags and links

--- Back-End/API-rocketnotes/knexfile.js
+++ Back-End/API-rocketnotes/knexfile.js
@@ -1,13 +1,12 @@
 const path = require("path");
+const connection = require("./src/database/knex");
+const { callbackify } = require("util");
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
