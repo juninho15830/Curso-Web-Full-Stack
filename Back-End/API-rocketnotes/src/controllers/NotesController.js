432799da3ca0c9ff526ddc8fc2d7387658726d9e
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-07-30 08:49:43 -0300

    update folder names

--- Back-End/API-rocketnotes/src/controllers/NotesController.js
+++ Back-End/API-rocketnotes/src/controllers/NotesController.js
@@ -1,7 +1,7 @@
 const knex = require("../database/knex");
 
-class NotesController{
-    async create(request, response){
+class NotesController {
+    async create(request, response) {
         const { title, description, tags, links } = request.body;
         const { user_id } = request.params;
 
@@ -31,6 +31,21 @@ class NotesController{
         await knex("tags").insert(tagsInsert);
 
         response.json();
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("notes").where({ id }).first();
+        const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+        const links = await knex("links").where({ note_id: id }).orderBy("created_at");
+
+        return response.json({
+           ...note,
+           tags,
+           links
+        });
+
     }
 }
 
