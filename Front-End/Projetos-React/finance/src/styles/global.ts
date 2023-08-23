6429f093383b3904bab539adede3879242bb2429
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-08-23 06:48:12 -0300

    update global styles

--- Front-End/Projetos-React/finance/src/styles/global.ts
+++ Front-End/Projetos-React/finance/src/styles/global.ts
@@ -2,7 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
-        --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        
+        --blue-light: #6933FF;
+        
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+        
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
     }
 
     * {
@@ -11,8 +20,36 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `
