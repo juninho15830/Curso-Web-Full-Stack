f02636fe3522b2fa4efebb973029a5a1631cd8ff
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-08-30 20:44:21 -0300

    update app

--- Front-End/Projetos-React/finance/src/components/Header/index.tsx
+++ Front-End/Projetos-React/finance/src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.png';
+import Modal from 'react-modal';
+
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="finance" />
-                <button>
+                <button type='button' onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
+
             </Content>
         </Container>
     )
