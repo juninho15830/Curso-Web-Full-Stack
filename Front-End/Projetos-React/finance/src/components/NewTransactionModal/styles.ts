f8a91f7cedf35cb6dc6e8088a949c59b08fcb677
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-08-31 20:14:16 -0300

    update styled modal

--- Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
+++ Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.form`
     h2 {
@@ -63,8 +64,10 @@ export const TransactionTypeContainer = styled.div`
         align-items: center;
         justify-content: center;
 
+        transition: border-color 0.2s;
+
         &:hover {
-            border-color: #aaa;
+            border-color: ${darken(0.1, '#d7d7d7')};
         }
 
         img {
