f74e2023b979163a876634226606b1c83d850eef
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-08-31 20:58:20 -0300

    finished styled modal

--- Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
+++ Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
     h2 {
@@ -52,35 +52,47 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
-
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
-
-        background: transparent;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        transition: border-color 0.2s;
-
-        &:hover {
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
-
-        img {
-            width: 20px;
-            height: 20px;
-        }
-
-        span {
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
-    }  
-
 `;
+
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor]) 
+        : 'transparent'
+    };
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
+    }
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
+`
