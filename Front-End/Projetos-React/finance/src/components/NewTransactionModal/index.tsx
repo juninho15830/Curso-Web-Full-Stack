115815f4ffcf97a0df65e5d00e2f1bdf96a41141
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-08-30 22:28:58 -0300

    update modal

--- Front-End/Projetos-React/finance/src/components/NewTransactionModal/index.tsx
+++ Front-End/Projetos-React/finance/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import closeImg from '../../assets/close.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +14,55 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
         <Modal 
             isOpen={isOpen} 
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Cadastrar</h2>
+
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            >
+
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    placeholder="Título" 
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                 />
+
+                <TransactionTypeContainer>
+                    <button
+                        type="button"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button
+                        type="button"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <input
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 }
