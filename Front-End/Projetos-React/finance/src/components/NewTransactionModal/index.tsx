d7fae88fec665f538d0fedf2862e1ab7485e1ba0
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-09-02 12:39:06 -0300

    Revert "update modal"
    This reverts commit 115815f4ffcf97a0df65e5d00e2f1bdf96a41141.

--- Front-End/Projetos-React/finance/src/components/NewTransactionModal/index.tsx
+++ Front-End/Projetos-React/finance/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -31,8 +31,6 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
         <Modal 
             isOpen={isOpen} 
             onRequestClose={onRequestClose}
-            overlayClassName="react-modal-overlay"
-            className="react-modal-content"
         >
 
             <button 
@@ -44,48 +42,36 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
                 <img src={closeImg} alt="Fechar modal" />
             </button>
 
-            <Container onSubmit={handleCreateNewTransaction}>
+            <Container>
                 <h2>Cadastrar transação</h2>
 
                 <input 
-                    placeholder="Título"
-                    value={title}
-                    onChange={event => setTitle(event.target.value)}
+                    placeholder="Título" 
                 />
 
                 <input 
                     type="number"
                     placeholder="Valor"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
                  />
 
                 <TransactionTypeContainer>
-                    <RadioBox
+                    <button
                         type="button"
-                        onClick={() => { setType('deposit'); }}
-                        isActive={type ==='deposit'}
-                        activeColor="green"
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </RadioBox>
+                    </button>
 
-                    <RadioBox
+                    <button
                         type="button"
-                        onClick={() => { setType('withdraw'); }}
-                        isActive={type === 'withdraw'}
-                        activeColor="red"
                     >
                         <img src={outcomeImg} alt="Saída" />
                         <span>Saída</span>
-                    </RadioBox>
+                    </button>
                 </TransactionTypeContainer>
 
                 <input
                     placeholder="Categoria"
-                    value={category}
-                    onChange={event => setCategory(event.target.value)}
                 />
 
                 <button type="submit">
--- Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
+++ Front-End/Projetos-React/finance/src/components/NewTransactionModal/styles.ts
@@ -52,47 +52,33 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+            border-color: #aaa;
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+    }  
+
 `;
-
-interface RadioBoxProps {
-    isActive: boolean;
-    activeColor: 'green' | 'red';
-}
-
-const colors = {
-    green: '#33cc95',
-    red: '#e52e4d'
-};
-
-export const RadioBox = styled.button<RadioBoxProps>`
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-
-    background: ${(props) => props.isActive 
-        ? transparentize(0.9, colors[props.activeColor]) 
-        : 'transparent'
-    };
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    transition: border-color 0.2s;
-
-    &:hover {
-        border-color: ${darken(0.1, '#d7d7d7')};
-    }
-
-    img {
-        width: 20px;
-        height: 20px;
-    }
-
-    span {
-        display: inline-block;
-        margin-left: 1rem;
-        font-size: 1rem;
-        color: var(--text-title);
-    }
-`
