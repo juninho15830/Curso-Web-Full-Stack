69d6eb62d867e0fd4ca824f2c628ba4fa2d397c7
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2023-09-09 14:34:28 -0300

    create context

--- Front-End/Projetos-React/finance/src/App.tsx
+++ Front-End/Projetos-React/finance/src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -20,7 +21,7 @@ export function App() {
     }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
